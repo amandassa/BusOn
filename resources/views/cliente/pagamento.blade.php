90a1b40be264d3fd7e9e6d61cccbb33950663bb2
Antony Araujo <contato.antonyaraujo@gmail.com>
2021-10-05 01:15:57 -0300

    Ajuste de rotas e do estilo

--- resources/views/cliente/pagamento.blade.php
+++ resources/views/cliente/pagamento.blade.php
@@ -3,8 +3,15 @@
 @section('title', 'Pagamento - ')
 
 @section('content')
-    <div class="card">
-           
-    </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="card card-default">
+                <div class="card-header"><h5>Informações Principais</h5></div>
+                <div class="card-body">               
+                    Alo alo
+                </div>
+            </div>
+        </div>
+    </section>
     
 @endsection
