870a5165c7bc12cc9d9d9aaa6b8200d49407bd48
Antony Araujo <contato.antonyaraujo@gmail.com>
2021-10-05 11:36:29 -0300

    Ajustando o blade basico

--- resources/views/cliente/pagamento.blade.php
+++ resources/views/cliente/pagamento.blade.php
@@ -3,10 +3,10 @@
 @section('title', 'Pagamento - ')
 
 @section('content')
+    <h3>Forma de Pagamento</h3>
     <section class="content">
         <div class="container-fluid">
-            <div class="card card-default">
-                <div class="card-header"><h5>Informações Principais</h5></div>
+            <div class="card card-default">                
                 <div class="card-body">               
                     Alo alo
                 </div>
