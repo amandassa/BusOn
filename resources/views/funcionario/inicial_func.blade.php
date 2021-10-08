baba9679c9e47debae09d91e2d98e3b0cd26f9d4
ozenilsonalisson <ozenilsonalisson@gmail.com>
2021-10-08 19:47:21 -0300

    adicao de tela para testagem de adicao de arquivo

--- resources/views/funcionario/inicial_func.blade.php
+++ resources/views/funcionario/inicial_func.blade.php
@@ -5,33 +5,6 @@
 @section('content')
     <link href="/css/estiloPaginas.css" rel="stylesheet">
     <div class="container">
-          <b><p>Dashboard</p></b>
-          <img src="" alt="">
-
-          <br>
-          
-          <b><p>Estatisticas</p></b>
-          <div class="row"> <!-- Linha 1 -->
-              <div class="quadrado">
-              </div>
-
-              <div class="quadrado">  
-              </div>
-
-              <div class="quadrado">        
-              </div>
-          </div>
-
-          <div class="row"> <!-- Linha 1 -->
-              <div class="quadrado">
-              </div>
-
-              <div class="quadrado">  
-              </div>
-
-              <div class="quadrado">        
-              </div>
-              
-          </div>     
+       
     </div>
 @endsection
