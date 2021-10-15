58baa28f8215be131352785a8e67e7946fb417d9
lucasxgb <lgabriel1607@gmail.com>
2021-10-15 14:40:28 -0300

    confirmar

--- resources/views/administrador/gerenciaUsuarios.blade.php
+++ resources/views/administrador/gerenciaUsuarios.blade.php
@@ -4,63 +4,135 @@
 
 @section('content')
 <link href="/css/estiloGerencia.css" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
       <div class="container" id="contPrincipal">
         <div id="meio">
           <div class="cimaGu">
-            <input type="text" placeholder="Diga o que busca aqui..." id="buscaGu">
+            <div class="buscGu">
+              <input type="text" class="busca" id="buscaGu" placeholder="Digite o que busca.">
+              <img src="/imagens/lupa.png" alt="Imagem Lupa" id="lupa">
+                <select name="buscaOpcoes" id="buscaOpcoes">
+                  <option value="nome">Nome</option>
+                  <option value="cpf">Cpf</option>
+                  <option value="email">Email</option>
+                 </select>            
+            </div>
             <button class="botaoAmarelo" id="botaoBusca"> Buscar </button>
-            <hr>
           </div>
-          <div class="row" id="colCentral">
-            <div class="col" id="esquerda">
+          <hr>
+          <div class="row" id="rowCentral">
+            <div class="col" id="colEsquerda">
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
             </div>
-            <div class="col" id="direita">
+            <div class="col" id="colDireita">
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
               <div class="card" id="cardsGu">
                 <div class="card-body" id="cardBodyGu">
-                  <button class="botaoAzul" id="botaoEditar"> Editar </button>
+                    <div class="divCentro" id="centroGu">
+                      <label for="staticNome">Nome: </label>
+                      <input type="text" readonly class="inputNome" id="staticNome" value="Jailson Ferreira Etcher">
+                      <label for="staticEmail">Email: </label>
+                      <input type="text" readonly class="inputEmail" id="staticEmail" value="etcher.jailson@gmail.com">
+                      <label for="staticCpf">Cpf: </label>
+                      <input type="text" readonly class="inputCpf" id="staticCpf" value="530.106.021-19">
+                      <button class="botaoAzul" id="botaoEditar"> Editar Informações</button>
+                    </div>      
                 </div>
               </div>
-              
             </div>
             
-            
           </div>
+          <hr>
           <div class="baixoGu">
             <button class="botaoAmarelo" id="botaoGu"> Baixar Arquivo Completo </button>
           </div>
