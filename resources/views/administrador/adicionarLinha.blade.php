6ad1d14bcc51c285903c133b57aeeaa35a606f3b
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2021-10-17 21:59:43 -0300

    criei a view de adicionar linha

---
+++ resources/views/administrador/adicionarLinha.blade.php
@@ -0,0 +1,12 @@
+@extends('app')
+
+@section('title', 'Cadastrar linha:')
+
+@section('content')
+<link href="/css/estiloGerencia.css" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+<div class="container">
+
+</div>
+@endsection
