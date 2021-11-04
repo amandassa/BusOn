fadf793987277a01e2239190f66a8a4f619676e6
israelbraitt <israelbraitt@protonmail.com>
2021-11-03 22:26:27 -0300

    atualiando AdministradorController

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCadastroFuncionarioRequest;
+use App\Http\Requests\StoreAlteracaoDadosFuncionarioRequest;
 use App\Http\Model\AdministradorModel as Adm;
 use Dotenv\Regex\Result;
 use Illuminate\Http\Request;
@@ -18,13 +19,16 @@ class AdministradorController extends Controller
             ->with('status', 'Funcionário Cadastrado com Sucesso!');
     }
 
-    
-
     // validador das informações do formulário da tela de criação do funcionário
     public function storeCadastroFuncionario(StoreCadastroFuncionarioRequest $request)
     {
         dd($request->all());
     }
 
+    // validador das informações do formulário da tela de alteração de dados do funcionário
+    public function storeCadastroFuncionario(StoreAlteracaoDadosFuncionarioRequest $request)
+    {
+        dd($request->all());
+    }
     
 }
