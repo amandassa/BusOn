7ae5200b489bfe724b10bcd3868446ba22744d2c
Anderson Lima <andersonlll007@gmail.com>
2021-11-04 23:17:34 -0300

    Criado os mecanismos de controle do fluxo das rotas, restaurado a função cadastrar cliente a como ela era anteriormente

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
@@ -15,7 +16,10 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            if(Auth::guard('funcionario')) 
+                return route('funcionarioLoginfront');
+            else
+                return route('login');
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,64 +12,72 @@
 |
 */
 
-
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
 Route::get('/', 'Auth\LoginController@showLoginForm');
 
+//Rotas restritas apenas para CLIENTES
+Route::group(['middleware' => ['auth:cliente']], function () {
+    Route::get('/inicio', function(){return view('cliente.inicio');})->name('inicio');
+    Route::any('/perfilCliente', 'ClienteController@index')->name('perfilCliente');
+    Route::post('/editarPerfilCliente', 'ClienteController@editarPerfil')->name('editarPerfilCliente');
+    Route::get('/selecao', function(){return view('cliente.selecao');})->name('selecao');
+    Route::get('/pagamento', function(){return view('cliente.partials.cartao');})->name('pagamento.cartao');
+    Route::get('/pagamento', function(){return view('cliente.pagamento')->with('valor', 1);})->name('pagamento');
+    Route::get('/pagamento/cartao', function(){return view('cliente._partials.cartao');})->name('cartao');
+    Route::get('/pagamento/boleto', function(){return view('cliente._partials.boleto');})->name('boleto');
+    Route::get('/confirmacao', function(){return view('cliente.confirmacao_pagamento.confirmacao');})->name('confirmacao');
+    Route::get('/confirmacaoB', function(){return view('cliente.confirmacao_pagamento.confirmacaoBoleto');})->name('confirmacaoBoleto');
+    Route::get('/recuperarAcessoCliente', function(){ return view('cliente.recuperarAcesso');})->name('recuperarAcessoCliente');
+});
 
-Route::get('/pagamento', function(){
-    return view('cliente.partials.cartao');
-    })->name('pagamento.cartao');
 
-Route::get('/pagamento', function(){
-    return view('cliente.pagamento')->with('valor', 1);
-    })->name('pagamento');
+//Rotas restritas apenas para FUNCIONARIOS
+Route::group(['middleware' => ['auth:funcionario']], function () {
+    Route::get('/inicialFuncionario', function(){return view('funcionario.inicial_func');})->name('inicial_func');
+    Route::get('/perfilFuncionario', function(){return view('funcionario.perfil');})->name('perfilFuncionario');
+    Route::get('/verificarLogs', function(){return view('funcionario.verificarLogs');})->name('verificarLogs');
+    Route::get('/geraRelat', function(){return view('funcionario.geraRelat');});
+    Route::get('/editarAgenda', function(){return view('funcionario.editarAgenda');});
+    Route::get('/recuperarAcessoFuncionario', function(){return view('funcionario.recuperarAcesso');})->name('recuperarAcessoFuncionario');
+}); 
+
+//Rotas restritas apenas para ADMNISTRADOR
+Route::group(['middleware' => ['adm']], function () {
+    Route::get('/inicialAdm', function(){return view('administrador.inicial_adm');})->name('inicial_adm');
+    Route::get('/perfilAdministrador', function(){return view('administrador.perfil');})->name('perfilAdministrador');
+    Route::get('/venderPassagens', function(){return view('administrador.vender_passagens');})->name('venderPassagens');
+    
+}); 
+
+
+
+
+
 
 Route::get('/base', function(){
     return view('app');
     })->name('base');
 
-Route::get('/confirmacao', function(){
-    return view('cliente.confirmacao_pagamento.confirmacao');
-    })->name('confirmacao');
 
-Route::get('/confirmacaoB', function(){
-    return view('cliente.confirmacao_pagamento.confirmacaoBoleto');
-    })->name('confirmacaoBoleto');
 
-Route::get('/selecao', function(){
-    return view('cliente.selecao');
-    })->name('selecao');
 
-Route::get('/cadastroclientes', function(){
-    return view('cliente.cadastroclientes');
-        })->name('cadastroclientes');
 
-Route::any('/perfilCliente', 'ClienteController@index')->name('perfilCliente');
 
-Route::post('/editarPerfilCliente', 'ClienteController@editarPerfil')->name('editarPerfilCliente');
 
-Route::get('/inicialAdm', function(){
-    return view('administrador.inicial_adm');
-    })->name('inicial_adm');
 
-Route::get('/inicialFuncionario', function(){
-    return view('funcionario.inicial_func');
-    })->name('inicial_func');
 
-Route::get('/verificarLogs', function(){
-    return view('funcionario.verificarLogs');
-    })->name('verificarLogs');
 
-Route::get('/perfilFuncionario', function(){
-    return view('funcionario.perfil');
-})->name('perfilFuncionario');
 
-Route::get('/perfilAdministrador', function(){
-    return view('administrador.perfil');
-})->name('perfilAdministrador');
+
+
+
+
+
+
+
 
 Route::get('/gerenciaUsuario_adm', function(){
     return view('administrador.gerenciaUsuarios');
@@ -88,30 +96,13 @@ Route::get('/adicionarTrecho', function(){
 })->name('adicionaTrecho');
 
 
-Route::get('/pagamento/cartao', function(){
-    return view('cliente._partials.cartao');
-})->name('cartao');
-
-Route::get('/pagamento/boleto', function(){
-    return view('cliente._partials.boleto');
-})->name('boleto');
-
-Route::get('/inicio', function(){
-    return view('cliente.inicio');
-})->name('inicio');
 
 
-Route::get('/geraRelat', function(){
-    return view('funcionario.geraRelat');
-});
 
-Route::get('/venderPassagens', function(){
-    return view('administrador.vender_passagens');
-})->name('venderPassagens');
 
-Route::get('/editarAgenda', function(){
-    return view('funcionario.editarAgenda');
-});
+
+
+
 
 Route::get('/cadastroFuncionario', function(){
     return view('administrador.cadastroFuncionario');
@@ -125,18 +116,14 @@ Route::get('/recuperarAcessoAdministrador', function(){
     return view('administrador.recuperarAcesso');
 })->name('recuperarAcessoAdministrador');
 
-Route::get('/recuperarAcessoFuncionario', function(){
-    return view('funcionario.recuperarAcesso');
-})->name('recuperarAcessoFuncionario');
 
-Route::get('/recuperarAcessoCliente', function(){
-    return view('cliente.recuperarAcesso');
-})->name('recuperarAcessoCliente');
+
 
 Route::get('/gerenciaUsuarios', function(){
     return view('administrador.gerenciaUsuarios');
 })->name('gerenciaUsuarios');
 
+//Rotas de autentificação de usuários.
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'clienteLogin'])->name('clienteLogin');
 Route::get('/login/funcionario', [App\Http\Controllers\Auth\LoginController::class, 'showFuncionarioLoginForm'])->name('funcionarioLoginfront');
 Route::post('/login/funcionario', [App\Http\Controllers\Auth\LoginController::class, 'funcionarioLogin'])->name('funcionarioLogin');
