7555e077bb66bdb378f56149c9d07a5f938d2246
Antony Araujo <contato.antonyaraujo@gmail.com>
2021-11-10 14:38:15 -0300

    Changes

---
+++ database/factories/LinhaFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Linha;
+use Faker\Generator as Faker;
+
+$factory->define(Linha::class, function (Faker $faker) {
+    return [
+        'direta' => rand(0, 1),
+        'total_vagas' => rand(2, 6)*10
+    ];
+});
---
+++ database/seeds/LinhaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Linha;
+
+class LinhaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::beginTransaction();
+        $linhas = factory(Linha::class, 10)->make()->toArray();            
+            foreach($linhas as $linha) {                
+                DB::statement('insert into linha(direta, total_vagas) values (?, ?)',
+                 [$linha['direta'], $linha['total_vagas']]);
+            }
+            DB::commit();
+    }
+}
