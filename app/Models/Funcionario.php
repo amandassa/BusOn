b6bef5e0d4bf836e06261ab8b8763165685e3d05
ozenilsonalisson <ozenilsonalisson@gmail.com>
2021-11-17 12:09:23 -0300

    correções nas estatisticas do funcionario

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use \DateTime;
+
 /*
 Nome: Funcionario (classe)
 Funcionalidade: Representa a entidade funcionário e as ações executadas pela mesma
@@ -95,6 +97,32 @@ class Funcionario extends Authenticatable {
             }
             
     }
+
+    /**
+     * Passagens vendidas por um funcionario no dia 
+     *
+     * @return void
+     */
+    public static function passagens_vendidas($mat_funcionario)
+    {
+        $data = new DateTime(); //Pega a data atual
+        
+        $data_hoje = $data->format('Y-m-d');
+        $data_uma_semana_atras = $data->modify('-7 day')->format('Y-m-d');  //Pega a data 7 dias antes
+        $data_uma_mes_atras = $data->modify('+7 day')->modify('-1 month')->format('Y-m-d'); //Pega a data 30 dias antes
+    
+        $qtd_vendas_hoje = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND $data_hoje = (SELECT data_compra FROM passagem WHERE venda.codigo_passagem = passagem.codigo)");
+        $qtd_vendas_7dias = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND ((SELECT data_compra FROM passagem WHERE venda.codigo_passagem =  passagem.codigo) BETWEEN $data_uma_semana_atras AND $data_hoje)"); 
+        $qtd_vendas_30dias = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND ((SELECT data_compra FROM passagem WHERE venda.codigo_passagem =  passagem.codigo) BETWEEN $data_uma_mes_atras AND $data_hoje)"); 
+
+        //checa se todas as listas tem algum valor
+        if(empty($qtd_vendas_hoje) || empty( $qtd_vendas_7dias) || empty($qtd_vendas_30dias)) {
+            return ['qtd_vendas_hoje' => 0, 'qtd_vendas_7dias' => 0, 'qtd_vendas_30dias' => 0];
+        }
+        return ['qtd_vendas_hoje' => $qtd_vendas_hoje[0]->contagem_vendas, 'qtd_vendas_7dias' => $qtd_vendas_7dias[0]->contagem_vendas, 'qtd_vendas_30dias' => $qtd_vendas_30dias[0]->contagem_vendas];
+    }
+
+
 
     /*public function getAuthPassword(){
         return $this->password;
--- app/Models/Linha.php
+++ app/Models/Linha.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use App\Models\Linha as Li;
 
 /*
 Nome: Linha (classe)
@@ -31,5 +32,59 @@ class Linha extends Model {
         $query = "SELECT dias_semana FROM linha WHERE ".$coluna." LIKE :cod";
         $data = DB::select($query, ['cod' => $parametro]);
         return $data;
+    }
+
+    /**
+     * Busca a linha mais vendida
+     * @return (cidade_partida e cidade_chegada) da linha mais vendida
+     */
+    public static function linha_mais_vendida ()
+    {
+        //busca a linha mais vendida no banco de dados
+        $linha = DB::select("SELECT codigo_linha, count(*) AS l FROM passagem GROUP BY codigo_linha HAVING count(*) = (SELECT max(l) FROM (SELECT codigo_linha, count(*) AS l FROM passagem GROUP BY codigo_linha) passagem)");
+        if(empty($linha)){
+            return ['linha_mais_vendida_partida'=> '', 'linha_mais_vendida_chegada'=> ''];
+        }
+        $linha_mais_vendida = Li::buscar_linha($linha[0]->codigo_linha);
+        
+        return ['linha_mais_vendida_partida'=> $linha_mais_vendida['cidade_partida'], 'linha_mais_vendida_chegada'=> $linha_mais_vendida['cidade_chegada']];
+
+    }
+    
+    /**
+     * Busca a linha menos vendida
+     * @return (cidade_partida e cidade_chegada) da linha menos vendida
+     */
+    public static function linha_menos_vendida ()
+    {
+        //busca a linha mais vendida no banco de dados
+        $linha = DB::select("SELECT codigo_linha, count(*) AS l FROM passagem GROUP BY codigo_linha HAVING count(*) = (SELECT min(l) FROM (SELECT codigo_linha, count(*) AS l FROM passagem GROUP BY codigo_linha) passagem)");
+        if(empty($linha)){
+            return ['linha_menos_vendida_partida'=> '', 'linha_menos_vendida_chegada'=> ''];
+        }
+        $linha_menos_vendida = Li::buscar_linha($linha[0]->codigo_linha);
+
+        return ['linha_menos_vendida_partida'=> $linha_menos_vendida['cidade_partida'], 'linha_menos_vendida_chegada'=> $linha_menos_vendida['cidade_chegada']];
+
+    }
+
+    /**
+     * Busca o nome da linha pelo codigo
+     * @return (cidade_partida e cidade_chegada)
+     */
+    public static function buscar_linha ($codigo_linha)
+    { 
+        //busca o nome da linha pelo codigo
+        $cidade_partida = DB::select("SELECT cidade_partida FROM trecho WHERE codigo = (select codigo_trecho from trechos_linha where codigo_linha = $codigo_linha and ordem = 1)");
+        $ordem = DB::select("SELECT max(ordem) as ordem from trechos_linha where codigo_linha = 1");
+        $cidade_chegada = DB::select("SELECT cidade_chegada FROM trecho WHERE codigo = (select codigo_trecho from trechos_linha where codigo_linha = $codigo_linha and ordem = :ordem)", ['ordem' => $ordem[0]->ordem]);
+        $total_passagens = DB::select("SELECT count(*) as total FROM passagem WHERE codigo_linha = $codigo_linha");
+
+        //checa se todas as listas tem algum valor
+        if(empty($total_passagens) || empty( $cidade_chegada) || empty($cidade_partida)) {
+            return ['total' => 0, 'cidade_partida' => '', 'cidade_chegada' => ''];
+        }
+
+        return ['total' => $total_passagens[0]->total, 'cidade_partida' => $cidade_partida[0]->cidade_partida, 'cidade_chegada' => $cidade_chegada[0]->cidade_chegada];
     }
 }
