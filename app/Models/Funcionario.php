982b9166c6f0e64a8d76984e70cb70776077efe9
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2021-11-19 18:17:16 -0300

    mudanças

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -4,13 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Http\Requests\AddVendaRequest;
 use Illuminate\Http\Request;
+use App\Http\Requests\AddVendaRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use \DateTime;
-
 /*
 Nome: Funcionario (classe)
 Funcionalidade: Representa a entidade funcionário e as ações executadas pela mesma
@@ -32,6 +30,7 @@ class Funcionario extends Authenticatable {
 
     protected $fillable = [
         'nome',
+        'matricula',
         'email',
         'CPF',
         'password',       
@@ -48,9 +47,11 @@ class Funcionario extends Authenticatable {
 
     public static function index(){ 
         $cpflogado = Auth::guard('funcionario')->user()->CPF;
+        
         $usuario = DB::select("select * from funcionario where CPF = ?", [$cpflogado])[0];
-        $fCpf = $usuario -> CPF;
-        $funCpf = substr($fCpf, 0, 3) . '.' . substr($fCpf, 3, 3) . '.' . substr($fCpf, 6, 3) . '-' . substr($fCpf, 9);
+
+
+        $funCpf = $usuario->CPF;
         $funNome = $usuario->nome;
         $funEmail = $usuario->email;
         $funMatricula = $usuario->matricula;
@@ -97,32 +98,6 @@ class Funcionario extends Authenticatable {
             }
             
     }
-
-    /**
-     * Passagens vendidas por um funcionario no dia 
-     *
-     * @return void
-     */
-    public static function passagens_vendidas($mat_funcionario)
-    {
-        $data = new DateTime(); //Pega a data atual
-        
-        $data_hoje = $data->format('Y-m-d');
-        $data_uma_semana_atras = $data->modify('-7 day')->format('Y-m-d');  //Pega a data 7 dias antes
-        $data_uma_mes_atras = $data->modify('+7 day')->modify('-1 month')->format('Y-m-d'); //Pega a data 30 dias antes
-    
-        $qtd_vendas_hoje = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND $data_hoje = (SELECT data_compra FROM passagem WHERE venda.codigo_passagem = passagem.codigo)");
-        $qtd_vendas_7dias = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND ((SELECT data_compra FROM passagem WHERE venda.codigo_passagem =  passagem.codigo) BETWEEN $data_uma_semana_atras AND $data_hoje)"); 
-        $qtd_vendas_30dias = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND ((SELECT data_compra FROM passagem WHERE venda.codigo_passagem =  passagem.codigo) BETWEEN $data_uma_mes_atras AND $data_hoje)"); 
-
-        //checa se todas as listas tem algum valor
-        if(empty($qtd_vendas_hoje) || empty( $qtd_vendas_7dias) || empty($qtd_vendas_30dias)) {
-            return ['qtd_vendas_hoje' => 0, 'qtd_vendas_7dias' => 0, 'qtd_vendas_30dias' => 0];
-        }
-        return ['qtd_vendas_hoje' => $qtd_vendas_hoje[0]->contagem_vendas, 'qtd_vendas_7dias' => $qtd_vendas_7dias[0]->contagem_vendas, 'qtd_vendas_30dias' => $qtd_vendas_30dias[0]->contagem_vendas];
-    }
-
-
 
     /*public function getAuthPassword(){
         return $this->password;
