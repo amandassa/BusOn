19afa943ea623f8721c0a3ba86f9d44aa6efd539
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2021-11-19 17:56:17 -0300

    import de vendaRequest

--- app/Http/Controllers/AdministradorController.php
+++ app/Http/Controllers/AdministradorController.php
@@ -1,94 +1,32 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreAddTrechoRequest;
-use App\Http\Requests\StoreCadastroFuncionarioRequest;
-use Illuminate\Support\Facades\DB;
-use App\Http\Requests\StoreAlteracaoDadosFuncionarioRequest;
-use App\Models\Administrador as Adm;
-use Dotenv\Regex\Result;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Hash;
-use App\Models\Funcionario;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AddVendaRequest;
+use App\Models\Funcionario as Func;
+use \DateTime;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
-class AdministradorController extends Controller
-{
-
-    public function criarFuncionario(StoreCadastroFuncionarioRequest $data)
-    {
-        Adm::criarFuncionario($request);
-        return redirect()->back()->with('message', 'Funcionário Cadastrado com Sucesso!');
-        /*
-        $cpf = str_replace(".", "", $request->cpf);
-        $cpf = str_replace("-", "", $cpf);
-        $nome = $request->nome;
-        $email = $request->email;
-        $is_admin = null;
-        if (isset($_POST['is_admin'])) {
-            $is_admin = '1';
-        } else {
-            $is_admin = '0';
-        }
-        $senha = Hash::make($request->senha);        
-        DB::statement('insert into funcionario(nome, CPF, email, password, is_admin) values (?, ?, ?, ?, ?)', [$nome, $cpf, $email, $senha, $is_admin]);
-        $funcionario = new Funcionario;
-        $request = [
-                 'nome' => $nome,
-                 'email' => $email,
-                 'CPF'  => $cpf,
-                 'password' => $senha,
-                 'is_admin' => $is_admin
-                 ];
-        $funcionario->fill($request);
-        
-        
-        */
-    }
-
-    // validador das informações do formulário da tela de criação do funcionário
-    public function storeCadastroFuncionario(StoreCadastroFuncionarioRequest $request)
-    {
-        dd($request->all());
-    }
-
-    /**
-     * Busca todos os Usuários do sistema
-     */
-    public function buscarFuncionarios ()
-    {              
-        
-        $funcionarios = DB::select("SELECT * FROM funcionario");
-        //return os usuários cadastrados no sistema;
-        return view('administrador.gerenciaFuncionarios', ['funcionarios'=>$funcionarios]);
-    }
-
-    /**
-     * Busca todos os Clientes do sistema
-     */
-    public function buscarClientes ()
-    {              
-        $clientes = DB::select("SELECT * FROM cliente");
-        //return os usuários cadastrados no sistema;
-        return view('administrador.gerenciaClientes', ['clientes'=>$clientes]);
-    }
+class FuncionarioController extends Controller {
 
     public function index(){
-        $administrador = Adm::index();
-        return view("administrador.perfil", ['administrador'=>$administrador]);
+        $funcionario = Func::index();
+        return view("funcionario.perfil", ['funcionario'=>$funcionario]);
     }
     
     public function editar(Request $request){
-        $adm = Adm::editar($request);
+        $funcionario = Func::editar($request);
 
-        if ($adm == 1) {
+        if ($funcionario == 1) {
             return redirect()
                         ->back()
                         ->with('error', 'Algum dos campos está vazio!, alteração não realizada');
-        } elseif ($adm ==2 ) {
+        } elseif ($funcionario ==2 ) {
             return redirect()
-                        ->route('perfilAdministrador.index')
+                        ->route('perfilFuncionario.index')
                         ->with('success', 'Perfil atualizado com sucesso!');
         } else {
             return redirect()
@@ -98,35 +36,41 @@ class AdministradorController extends Controller
         
     }
 
-    public function perfilFunc($email){
-        $funcionario = Adm::perfilFunc($email);
-        return view("administrador.editarPerfilFuncionario", ['funcionario'=>$funcionario]);
-    }
-    
-    public function editarFunc(Request $request){
+    /**
+     * Passagens vendidas por um funcionario no dia 
+     *
+     * @return void
+     */
+    public function passagens_vendidas()
+    {
+        $mat_funcionario= Auth::guard('funcionario')->user()->matricula; //pega a matricula do funcionario logado
         
-        $func = Adm::editarFunc($request);
+        $data = new DateTime(); //Pega a data atual
+        
+        $data_hoje = $data->format('Y-m-d');
+        $data_uma_semana_atras = $data->modify('-7 day')->format('Y-m-d');  //Pega a data 7 dias antes
+        $data_uma_mes_atras = $data->modify('+7 day')->modify('-1 month')->format('Y-m-d'); //Pega a data 30 dias antes
+    
+        $qtd_vendas_hoje = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND $data_hoje = (SELECT data_compra FROM passagem WHERE venda.codigo_passagem = passagem.codigo)");
+        $qtd_vendas_7dias = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND ((SELECT data_compra FROM passagem WHERE venda.codigo_passagem =  passagem.codigo) BETWEEN $data_uma_semana_atras AND $data_hoje)"); 
+        $qtd_vendas_30dias = DB:: select("SELECT COUNT(*) as contagem_vendas FROM venda WHERE venda.matricula_vendedor = $mat_funcionario AND ((SELECT data_compra FROM passagem WHERE venda.codigo_passagem =  passagem.codigo) BETWEEN $data_uma_mes_atras AND $data_hoje)"); 
 
-        if ($func['id'] == 1) {
-            return redirect()
-                        ->back()
-                        ->with('error', 'Algum dos campos está vazio!, alteração não realizada');
-        } elseif ($func['id'] ==2 ) {
-            return redirect()
-                        ->route('perfilAdministrador.perfilFunc', ['email' =>$func['email']])
-                        ->with('success', 'Perfil atualizado com sucesso!');
-        } else {
-            return redirect()
-                        ->back()
-                        ->with('error', 'As senhas não coincidem');
+        return view('funcionario.inicial_func')->with(['qtd_vendas_hoje' => $qtd_vendas_hoje[0]->contagem_vendas, 
+                                                        'qtd_vendas_7dias' => $qtd_vendas_7dias[0]->contagem_vendas,
+                                                        'qtd_vendas_30dias' => $qtd_vendas_30dias[0]->contagem_vendas]);
+    }
+
+    public function vender(AddVendaRequest $request){
+        $result = Func::venderPassagem($request);
+        if($result == 0){
+            return redirect()->back()->with('error', 'Sem vagas.');
+        }else{
+            return redirect()->back()->with('message', 'Venda Feita.');
         }
-        
-    }
 
-    public function storeCadastrarTrecho(StoreAddTrechoRequest $request){
-        return redirect('adicionarTrecho')->with('message', 'Trecho Cadastrado.');
+        //Func::venderPassagem($request);
     }
-    
-   
 
 }
+
+?>
