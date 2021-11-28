79c473cdd52f1cc5da0b24925bb69b2da4ca8427
amandassa <amandassa504@gmail.com>
2021-11-28 16:07:04 -0300

    alterações necessárias

--- database/factories/LinhaFactory.php
+++ database/factories/LinhaFactory.php
@@ -8,6 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(Linha::class, function (Faker $faker) {
     return [
         'direta' => rand(0, 1),
-        'total_vagas' => 60
+        'total_vagas' => 60,
+        'dias_semana' => rand(1,7),
+        'hora_partida' => $faker->time()
     ];
 });
--- database/seeds/LinhaSeeder.php
+++ database/seeds/LinhaSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use App\Models\Linha;
 
@@ -15,8 +16,8 @@ class LinhaSeeder extends Seeder
         DB::beginTransaction();
         $linhas = factory(Linha::class, 10)->make()->toArray();            
             foreach($linhas as $linha) {                
-                DB::statement('insert into linha(direta, total_vagas) values (?, ?)',
-                 [$linha['direta'], $linha['total_vagas']]);
+                DB::statement('insert into linha(direta, total_vagas, dias_semana, hora_partida) 
+                values (?, ?, ?, ?)', [$linha['direta'], $linha['total_vagas'], $linha['dias_semana'], $linha['hora_partida']]);
             }
         DB::commit();
     }
