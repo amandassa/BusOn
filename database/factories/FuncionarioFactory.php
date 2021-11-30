efcdb132be6edf640db62ca9e66043fd6b5a07d0
Antony Araujo <contato.antonyaraujo@gmail.com>
2021-11-30 00:39:12 -0300

    Emails gerados conforme o nome dos funcionarios e clientes sem erros

--- database/factories/FuncionarioFactory.php
+++ database/factories/FuncionarioFactory.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Hash;
 $factory->define(Funcionario::class, function (Faker $faker) {
     $email = $faker->email();
     $email = explode("@", $email);
-    $nome = $faker->firstName();
+    $nome_gerado = $faker->firstName();
+    $nome = preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"), $nome_gerado);
     return [
         'nome' => $nome,
         'email' => strtolower($nome).'@'.$email[1],
