170abaee25b60a2027d59ed13fb6cfb7d62256b8
Anderson Lima <andersonlll007@gmail.com>
2021-11-29 22:39:14 -0300

    Alterações na tela de pagamento e confirmação do pedido

--- app/Models/Linha.php
+++ app/Models/Linha.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use App\Models\Linha;
 
 /*
 Nome: Linha (classe)
---
+++ app/Models/Pagamento_boleto.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pagamento_boleto extends Model
+{
+    public static function criar($codigo_barras, $nome, $cpf, $codigo_pagamento)
+    {
+       $confirmacao = DB::insert('INSERT INTO pagamento_boleto (codigo_barras, nome, cpf, codigo_pagamento) VALUES (?,?,?,?)', [$codigo_barras, $nome, $cpf, $codigo_pagamento]);
+        return $confirmacao;         
+    }
+    
+    public static function codigoBarrasDisponivel($codigo_barras)
+    {
+        $query = "SELECT codigo_barras FROM pagamento_boleto";
+        $codigos = DB::select($query, ['codigo_barras' => $codigo_barras]);
+        foreach ($codigos as $codigo)
+        {
+            if($codigo_barras == $codigo->codigo_barras) 
+                return false;
+        }
+        return true;
+
+    }
+
+}
