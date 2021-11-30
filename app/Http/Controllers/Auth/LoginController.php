6e72fbf645b39fdb4657aed92a0dd82a0d0c35a4
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2021-11-30 20:44:25 -0300

    erro na importação do modelo de log

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
-use App\Models\Log as Logs;
+use App\Models\Log;
 
 class LoginController extends Controller
 {
