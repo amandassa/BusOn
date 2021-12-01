e7c2b7d96d5b5d7e5ddc49cb115de2076d7d295f
israelbraitt <israelbraitt@protonmail.com>
2021-11-30 23:43:14 -0300

    adicionando métodos de log para editar linha e trecho

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -7,17 +7,34 @@ use Illuminate\Support\Facades\DB;
 
 class Log extends Model
 {
-    public static function acessoCliente($email, $date){
+    public static function acessoCliente($email, $data_hora){
         $cliente_info = DB::select("SELECT cpf, nome FROM cliente WHERE email = ?;", [$email])[0];
         $cpf = $cliente_info->cpf;
         $nome = $cliente_info->nome;
 
         DB::insert("INSERT INTO logs (cpf_usuario, descricao, data_hora, tipo_usuario) 
-        VALUES (?, 'Cliente ? entrou no site', ?, 'C');", [$cpf, $nome, $date]);
+        VALUES (?, 'Cliente ? entrou no site', ?, 'C');", [$cpf, $nome, $data_hora]);
     }
 
-    public static function trechoAdicionado($cpf, $nome, $date, $nome_trecho){
+    public static function trechoAdicionado($cpf, $nome, $data_hora, $nome_trecho){
         DB::insert("INSERT INTO logs (cpf_usuario, descricao, data_hora, tipo_usuario) 
-        VALUES (?, 'Administrador ? adicinou o trecho ?', ?, 'A');", [$cpf, $nome, $nome_trecho, $date]);
+        VALUES (?, 'Administrador ? adicionou o trecho ?', ?, 'A');", [$cpf, $nome, $nome_trecho, $data_hora]);
     }
+
+    public static function editarLinha($cod_linha, $data_hora){
+        $cpf = Auth::guard('cliente')->user()->cpf;
+        $nome = Auth::guard('cliente')->user()->nome;
+
+        DB::insert("INSERT INTO logs (cpf_usuario, tipo_usuario, descricao, data_hora)
+        VALUES (?, 'A', 'Funcionario ? editou a linha ?', ?);", [$cpf, $nome, $cod_linha, $data_hora]);
+    }
+
+    public static function editarTrecho($cod_trecho, $data_hora){
+        $cpf = Auth::guard('cliente')->user()->cpf;
+        $nome = Auth::guard('cliente')->user()->nome;
+
+        DB::insert("INSERT INTO logs (cpf_usuario, tipo_usuario, descricao, data_hora)
+        VALUES (?, 'F', 'Funcionario ? editou o trecho ?', ?);", [$cpf, $nome, $cod_trecho, $data_hora]);
+    }
+
 }
