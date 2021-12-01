0f12d08b085d608be387e20609ec6d6a94f105c4
israelbraitt <israelbraitt@protonmail.com>
2021-12-01 00:52:20 -0300

    adicionando comentários no model de lods

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -7,30 +7,48 @@ use Illuminate\Support\Facades\DB;
 
 class Log extends Model
 {
+    /**
+     * Pesquisa todos os logs no banco de dados
+     * 
+     * @return $logs todos os logs da tabela no banco de dados
+     */
     public static function pesquisarTodosLogs(){
         $logs = DB::select("SELECT * FROM logs");
 
         return $logs;
     }
 
+    /**
+     * Pesquisa os logs no banco de dados de acordo com um cpf de um usuário
+     * 
+     * @param $cpf cpf do usuário
+     * @return $logs os logs da tabela no banco de dados de acordo com um cpf de um usuário
+     */
     public static function pesquisarPorCPF($cpf){
-        $logs = DB::select("SELECT * FROM logs WHERE cpf=$cpf");
+        $logs = DB::select("SELECT * FROM logs WHERE cpf = $cpf");
 
         return $logs;
     }
 
+    /**
+     * Pesquisa os logs no banco de dados de acordo com um intervalo de datas
+     * 
+     * @param $data_hora_inicio data de início do intervalo de tempo
+     * @param $data_hora_fim data de fim do intervalo de tempo
+     * @return $logs os logs da tabela no banco de dados de acordo com um intervalo de datas
+     */
     public static function pesquisarPorData($data_hora_inicio, $data_hora_fim){
         $logs = DB::select("SELECT * FROM logs WHERE data_hora BETWEEN $data_hora_inicio AND $data_hora_fim");
 
         return $logs;
     }
 
-    public static function pesquisarPorData($data_hora_inicio, $data_hora_fim){
-        $logs = DB::select("SELECT * FROM logs WHERE data_hora BETWEEN $data_hora_inicio AND $data_hora_fim");
-
-        return $logs;
-    }
-
+    /**
+     * Registar logs de acesso (login) dos clientes ao sistema
+     * 
+     * @param $email email do cliente
+     * @param $data_hora data e hora do acesso ao sistema
+     */
     public static function acessoCliente($email, $data_hora){
         $cliente_info = DB::select("SELECT cpf, nome FROM cliente WHERE email = ?;", [$email])[0];
         $cpf = $cliente_info->cpf;
@@ -40,11 +58,25 @@ class Log extends Model
         VALUES (?, 'Cliente ? entrou no site', ?, 'C');", [$cpf, $nome, $data_hora]);
     }
 
+    /**
+     * Registrar logs quando um trecho for adicionado
+     * 
+     * @param $cpf cpf do funcionário
+     * @param $nome nome do funcionario
+     * @param $data_hora data e hora da adição do trecho
+     * @param $nome_trecho nome do trecho
+     */
     public static function trechoAdicionado($cpf, $nome, $data_hora, $nome_trecho){
         DB::insert("INSERT INTO logs (cpf_usuario, descricao, data_hora, tipo_usuario) 
         VALUES (?, 'Administrador ? adicionou o trecho ?', ?, 'A');", [$cpf, $nome, $nome_trecho, $data_hora]);
     }
 
+    /**
+     * Registrar logs quando uma linha for editada
+     * 
+     * @param $cod_linha código da linha editada
+     * @param $data_hora data e hora da edição
+     */
     public static function editarLinha($cod_linha, $data_hora){
         $cpf = Auth::guard('cliente')->user()->cpf;
         $nome = Auth::guard('cliente')->user()->nome;
@@ -53,6 +85,12 @@ class Log extends Model
         VALUES (?, 'A', 'Funcionario ? editou a linha ?', ?);", [$cpf, $nome, $cod_linha, $data_hora]);
     }
 
+    /**
+     * Resgistrar logs quando um trecho for editado
+     * 
+     * @param $cod_trecho código do trecho editado
+     * @param $data_hora data e hora da edição
+     */
     public static function editarTrecho($cod_trecho, $data_hora){
         $cpf = Auth::guard('cliente')->user()->cpf;
         $nome = Auth::guard('cliente')->user()->nome;
